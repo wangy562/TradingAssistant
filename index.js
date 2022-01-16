63ac15a8fba15ed8a8915931a62d759874e5a457
wangy562 <andyyqwang@outlook.com>
2022-01-15 21:13:25 -0800

    use nodemon and fix minor syntax errors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express"
-import router from "./routes/emails.js"
+import { router } from "./routes/emails.js"
 import dotenv from "dotenv"
 
 dotenv.config()
 const app = express() 
+const port = 8080
 
 app.use("/api/emails", router)
 
