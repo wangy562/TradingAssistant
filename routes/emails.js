50ff7c54684e4aa2226bbde0c2588fc442503581
wangy562 <andyyqwang@outlook.com>
2022-01-15 21:02:51 -0800

    finish email routes

--- routes/emails.js
+++ routes/emails.js
@@ -12,7 +12,20 @@ router.get("/:id", (req, res) => {
     console.log(`email identified by ${req.params.id}`)
 })
 
+router.delete("/delete_email", (req, res) => {
+    // delete specified email
+    
+})
+
 router.post("/add_email", (req, res) => {
     // add a new email
 
+})
+
+router.get("/:id/following", (req, res) => {
+    // get followed tickers list for user by ID
+})
+
+router.put("/:id/update_following", (req, res) => {
+    // update followed tickers list for user by ID
 })
