694bab4aec001d5415000b792699aae5c8e6d3bd
wangy562 <andyyqwang@outlook.com>
2022-01-16 23:46:12 -0800

    add accounts to stream from

--- middleware/tweetProcessor.js
+++ middleware/tweetProcessor.js
@@ -6,3 +6,6 @@ dotenv.config()
 const api_key = process.env.TWT_API_KEY
 const api_secret = process.env.TWT_API_SECRET
 const bearer = process.env.BEARER 
+
+// list of users to stream from
+const streamUsernames = ["unusual_whales", "Deltaone"]
