76f013f1647244eb490b1789931fff8342e3fd1a
wangy562 <andyyqwang@outlook.com>
2022-01-16 23:48:12 -0800

    fix env file

--- middleware/tweetProcessor.js
+++ middleware/tweetProcessor.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 const api_key = process.env.TWT_API_KEY
 const api_secret = process.env.TWT_API_SECRET
-const bearer = process.env.BEARER 
+const bearer = process.env.TWT_BEARER_TOKEN 
 
 // list of users to stream from
 const streamUsernames = ["unusual_whales", "Deltaone"]
