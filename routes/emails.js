a809c2f4fa1393f943183eb3891face67f307047
wangy562 <andyyqwang@outlook.com>
2022-02-06 18:16:53 -0800

    write controller logic

--- routes/emails.js
+++ routes/emails.js
@@ -1,31 +1,58 @@
 import express from "express"
+import { UserModel } from "../models/user.js"
 
 export const router = express.Router() 
 
-router.get("/", (req, res) => {
-    // get all email subscribers
-    console.log("all emails")
-})
-
 router.get("/:id", (req, res) => {
-    // get email by ID
-    console.log(`email identified by ${req.params.id}`)
+    const { userId } = req.params
+    UserModel.findById(userId, (err, user) => {
+        if (err) {
+            res.status(404).send(err)
+        } else {
+            res.status(200).json(user)
+        }
+    })
 })
 
-router.delete("/delete_email", (req, res) => {
-    // delete specified email
-    
+router.delete("/delete_email/:email", (req, res) => {
+    const { email } = req.params
+    UserModel.findOneAndDelete({ email: email }, (err, user) => {
+        if (err) {
+            res.status(404).send(err)
+        } else {
+            res.status(200).json(user)
+        }
+    })
 })
 
-router.post("/add_email", (req, res) => {
-    // add a new email
-
+router.post("/add_email/:email/", (req, res) => {
+    const { email } = req.params
+    const { tickers } = req.body
+    const user = new UserModel({email: email, tickers: tickers})
+    await user.save()
+    //TODO validate user has been added
+    res.status(200).json(user)
 })
 
 router.get("/:id/following", (req, res) => {
-    // get followed tickers list for user by ID
+    const { userId } = req.params
+    const user = UserModel.findById(userId, (err, user) => {
+        if (err) {
+            res.status(404).send(err)
+        } else {
+            res.status(200).json(user.tickers)
+        }
+    })
 })
 
 router.put("/:id/update_following", (req, res) => {
-    // update followed tickers list for user by ID
+    const { userId } = req.params
+    const { tickers } = req.body
+    const user = UserModel.findByIdAndUpdate(userId, (err, user) => {
+        if (err) {
+            res.status(404).send(err)
+        } else {
+            res.status(200)
+        }
+    } )
 })
